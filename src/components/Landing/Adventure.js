5ef1ace0625ac60db4cc994d744f009e909dcfc6
isaiah usiholo <evansaizik@gmail.com>
2022-11-11 13:20:48 +0100

    styling the adventure components

--- src/components/Landing/Adventure.js
+++ src/components/Landing/Adventure.js
@@ -4,19 +4,25 @@ import desertKing from '../Assets/Group 4048.svg';
 import desert from '../Assets/Group 4049.svg';
 import King from '../Assets/Group 4050.svg';
 import sea from '../Assets/Group 4051.svg';
+import desertKingOne from '../Assets/Group 40481.svg';
+import desertOne from '../Assets/Group 40491.svg';
+import KingOne from '../Assets/Group 40501.svg';
+import seaOne from '../Assets/Group 40511.svg';
+
 
 const Adventure = () => {
   return <section className={classes.container} >
     <h3>Inspiration for your next adventure</h3>
     <main className={classes.main}>
-      <div className={classes.offers}>
+      <div>
         <img src={desertKing} alt='desert king' />
         <img src={desert} alt='desert' />
         <img src={King} alt='king' />
         <img src={sea} alt='sea' />
-      </div>
-      <div className={classes.offers}>
-
+        <img src={desertKingOne} alt='des' />
+        <img src={desertOne} alt='des' />
+        <img src={KingOne} alt='des' />
+        <img src={seaOne} alt='des' />
       </div>
     </main>
   </section>
