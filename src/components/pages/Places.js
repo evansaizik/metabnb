39ac8021496ea3fc05293299600f3d13b4484613
isaiah usiholo <evansaizik@gmail.com>
2022-11-12 15:19:17 +0100

    working on the layout

--- src/components/pages/Places.js
+++ src/components/pages/Places.js
@@ -4,22 +4,22 @@ import one from '../Assets/Frame 140.svg';
 import two from '../Assets/Frame 141.svg';
 import three from '../Assets/Frame 141.svg';
 import four from '../Assets/Frame 141.svg';
-import five from '../Assets/Frame 141.svg';
-import six from '../Assets/Frame 141.svg';
-import seven from '../Assets/Frame 141.svg';
-import eight from '../Assets/Frame 141.svg';
-import nine from '../Assets/Frame 141.svg';
-import ten from '../Assets/Frame 141.svg';
-import el from '../Assets/Frame 141.svg';
-import twe from '../Assets/Frame 141.svg';
-import thir from '../Assets/Frame 141.svg';
-import fort from '../Assets/Frame 141.svg';
-import fift from '../Assets/Frame 141.svg';
-import sixt from '../Assets/Frame 141.svg';
+// import five from '../Assets/Frame 141.svg';
+// import six from '../Assets/Frame 141.svg';
+// import seven from '../Assets/Frame 141.svg';
+// import eight from '../Assets/Frame 141.svg';
+// import nine from '../Assets/Frame 141.svg';
+// import ten from '../Assets/Frame 141.svg';
+// import el from '../Assets/Frame 141.svg';
+// import twe from '../Assets/Frame 141.svg';
+// import thir from '../Assets/Frame 141.svg';
+// import fort from '../Assets/Frame 141.svg';
+// import fift from '../Assets/Frame 141.svg';
+// import sixt from '../Assets/Frame 141.svg';
 
 
 const Places = () => {
-  const houses = []
+  // const houses = []
 
 
   return <div className={styles.page}>
@@ -34,9 +34,14 @@ const Places = () => {
       <p>Farm</p>
     </div>
     <section className={styles.place}>
-      <div>
-        <img src={one} alt='places' />
-      </div>
+      {/* <img src={one} alt='places' />
+      <img src={two} alt='places' />
+      <img src={three} alt='places' />
+      <img src={four} alt='places' /> */}
+      <div><img src={four} alt='places' /></div>
+      <div><img src={four} alt='places' /></div>
+      <div>hello</div>
+      <div>hello</div>
     </section>
   </div>
 }
