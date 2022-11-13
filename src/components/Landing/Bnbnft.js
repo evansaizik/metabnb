8007e0859c191ee85ec5fa0fb088163854134595
isaiah usiholo <evansaizik@gmail.com>
2022-11-13 13:45:38 +0100

    mobile and tab design implementation

--- src/components/Landing/Bnbnft.js
+++ src/components/Landing/Bnbnft.js
@@ -1,17 +1,15 @@
 import styles from './Bnbnft.module.css';
 
-import NFTone from '../Assets/image 7.svg';
-import NFTtwo from '../Assets/image 8.svg';
-import NFTthree from '../Assets/image 9.svg';
+import NFTbox from '../Assets/NFTbox.svg';
+// import NFTtwo from '../Assets/image 8.svg';
+// import NFTthree from '../Assets/image 9.svg';
 
 
 const Bnbnft = () => {
   return <div className={styles.container}>
-    <section className={styles.frame}>
-      <img id={styles.one} src={NFTone} alt='NFT' />
-      <img id={styles.two} src={NFTtwo} alt='NFT' />
-      <img id={styles.three} src={NFTthree} alt='NFT' />
-    </section>
+    <>
+      <img id={styles.frame} src={NFTbox} alt='NFT' />
+    </>
     <section>
       <h2 className={styles.h2}>Metabnb NFTs</h2>
       <p className={styles.p}>Discover our NFT gift cards collection. Loyal customers get amazing gift cards which are traded as NFTs. These NFTs gives our customer access to loads of exclusive services</p>
--- src/components/Landing/Nav.js
+++ src/components/Landing/Nav.js
@@ -3,14 +3,10 @@ import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
   return <nav className={classes.nav}>
-    {/* <span><a href='#'>Home</a></span> */}
     <NavLink to='/'>Home</NavLink>
     <NavLink to='/places' >Place to stay</NavLink>
     <NavLink to='/' >NFTs</NavLink>
     <NavLink to='/' >Community</NavLink>
-
-    {/* <span><a href='#'>NFTs</a></span> */}
-    {/* <span><a href='#'>Community</a></span> */}
   </nav>
 };
 
